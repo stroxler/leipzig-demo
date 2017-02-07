ebd1ea6b8182a712b6dcb208c3a402a18a847369
Steven Troxler <steven.troxler@gmail.com>
2017-02-06 17:29:53 -0800

    Add an organ to the track
    This demonstrates using the progression for multiple instruments,
    using control structures to modify things like lowpass filters,
    and playing multiple notes at once in leipzig.

--- src/leipzig_demo/core.clj
+++ src/leipzig_demo/core.clj
@@ -3,7 +3,8 @@
             [leipzig.melody :refer :all]
             [leipzig.scale :as scale]
             [leipzig.temperament :as temperament]
-            [leipzig.live :as live]))
+            [leipzig.live :as live]
+            [leipzig.chord :as chord]))
 
 ;; utilities --------------------
 
@@ -18,25 +19,48 @@
 
 ;; the instruments and methods -------------------
 
-(definst inst:bass [freq 110]
-  (-> freq
+;; bass
+
+(definst inst:bass [hz 110]
+  (-> hz
       saw
-      (rlpf (line:kr (* freq 10) freq 1))
-      (* (env-gen (perc 0.1 0.4) :action FREE))))
+      (rlpf (line:kr (* hz 10) hz 1))
+      (* (env-gen (perc 0.1 0.4) :action FREE))
+      (* 0.50)))
 
-;;(defmethod live/play-note :bass [{hertz :pitch}]
-;;  (inst:bass hertz))
+(defmethod live/play-note :bass [{hertz :pitch}]
+  (inst:bass hertz))
 
-(definst inst:kick [freq 90]
-  (-> (line:kr freq (* freq 0.5) 0.5)
+;; kick
+
+(definst inst:kick [hz 90]
+  (-> (line:kr hz (* hz 0.5) 0.5)
       sin-osc
-      (+ (sin-osc freq))
+      (+ (sin-osc hz))
       (* (env-gen (perc 0.01 0.3) :action FREE))))
 
 (defmethod live/play-note :kick [{hertz :pitch}]
   (inst:kick))
 
+;; organ
+
+(definst inst:organ [hz 110  sec 10]
+  (let [waver-slow (sin-osc:kr 3)
+        waver-fast (sin-osc:kr 4)]
+    (->
+     (+ (* 0.1 (sin-osc hz)) (* 0.05 (saw hz)))
+     (rlpf (mul-add waver-fast 300 (* hz 4)))
+     (rlpf (mul-add waver-slow 400 (* hz 3)))
+     (* (env-gen (adsr) (line:kr 1 0 sec) :action FREE)))))
+
+(defmethod live/play-note :organ [{hertz :pitch seconds :duration}]
+  (inst:organ hertz seconds))
+
 ;; the parts --------------------
+
+(def chord-progression:roots [0 0 3 0 4 0])
+
+;; bass
 
 (defn part-bass:riff [root]
   (->>
@@ -45,9 +69,10 @@
    (where :pitch (comp scale/lower scale/lower))
    (adding :part :bass)))
 
-(def part-bass:progression [0 0 3 0 4 0])
 
-(def part:bass (mapthen part-bass:riff part-bass:progression))
+(def part:bass (mapthen part-bass:riff chord-progression:roots))
+
+;; kick beat
 
 (def part:kick
   (->>
@@ -56,12 +81,22 @@
    (times 12)
    (adding :part :kick)))
 
+;; organ
+
+(defn part-organ:play-chord [root]
+  (->> (phrase (repeat 8) [(-> chord/triad (chord/root root))]) ; (8 is duration, holding 2 measures)
+       (adding :part :organ)))
+
+(def part:organ (mapthen part-organ:play-chord chord-progression:roots))
+
+
 ;; the track --------------
 
 (def track:blues
   (->>
    part:bass
    (with part:kick)
+   (with part:organ)
    (where :pitch (comp temperament/equal scale/E scale/minor))
    (where :time (bpm 90))
    (where :duration (bpm 90))))
@@ -70,18 +105,14 @@
   ;; overtone stuff
   (inst:bass)
   (inst:kick)
+  (inst:organ)
   (stop)
-  (take-cycle 2 [1 2 3])
-  (cycle-times 2 [1 2 3])
   )
 
 (comment
-  (part-bass:riff 5)
-  part:bass
-  part:kick
   track:blues
   ;; leipzig stuff
   (live/jam (var track:blues))
   (live/stop)
-  (live/play track)
+  (live/play track:blues)
   )
